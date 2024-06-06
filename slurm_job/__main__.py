d93b313ced0cab35eababe34a18e26a00333ba6e
yuema137 <3124558229@qq.com>
2024-06-06 18:10:05 -0500

    Refactor main function in __main__.py

--- slurm_job/__main__.py
+++ slurm_job/__main__.py
@@ -1,20 +1,6 @@
-import argparse
+"""Main entry point for the slurm_job package."""
 
-from slurm_job.slurm import watch_slurm_jobs
-
-
-def main():
-    parser = argparse.ArgumentParser(description="Watch a directory for Slurm job scripts.")
-    parser.add_argument(
-        "directory",
-        type=str,
-        help="Directory to watch for Slurm job scripts.",
-        default="slurm_jobs",
-    )
-    args = parser.parse_args()
-
-    watch_slurm_jobs(args.directory)
-
+from slurm_job.slurm import main
 
 if __name__ == "__main__":
     main()
